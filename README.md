d07bfd109eeed6b7225d7849d1366aed6b7a6100
jennapfingsten <jennapfingsten17@gmail.com>
2023-03-19 21:43:03 -0500

    Shell for the app

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a program designed to send automated birthday messages on Groupme
---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useEffect } from "react";
+import "./App.css";
+
+function App() {
+	const [data, setData] = React.useState(null);
+
+	useEffect(() => {
+		fetch("/api")
+			.then((res) => res.json())
+			.then((data) => setData(data.message));
+	}, []);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>{!data ? "Loading..." : data}</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
